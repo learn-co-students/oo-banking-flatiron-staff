49bf80f3ab1b0c9dbbf5ce51f1fccb86c36392a5
Rupa <rupa211984@gmail.com>
2019-10-31 22:37:17 -0700

    Pass tests

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? && @sender.balance >= @amount
+  end
+
+  def execute_transaction
+    return if @status == 'complete'
+
+    unless valid?
+      @status = 'rejected'
+      return "Transaction rejected. Please check your account balance."
+    end
+
+    transfer(1, 'complete')
+  end
+
+  def reverse_transfer
+    return unless @status == 'complete'
+
+    transfer(-1, 'reversed')
+  end
+
+  private
+
+  def transfer(direction, status)
+    @sender.deposit(-1 * direction * @amount)
+    @receiver.deposit(direction * @amount)
+    @status = status
+  end
 end
